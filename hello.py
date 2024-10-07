25d9cd076a8f3b01d913df3ab74a4998fe21fe07
Lamine <lamined34@gmail.com>
2024-10-07 15:36:49 +0000

    branch tmld

--- hello.py
+++ hello.py
@@ -1,5 +1,2 @@
 if __name__ == "__main__":
-    print("Hello world!")
-    print("I am Lamine Diallo")
-    print("hi! ho! he!") #Modified by another one
-    print("I am actually happy")
+    print("Simplified one!")
